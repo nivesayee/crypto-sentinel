4c05662ab01f501bb2042bdd4781fcc1af8c00cc
nivesayee <nivesayee@gmail.com>
2024-10-16 15:29:18 +0100

    Sentiment analysis

--- ingestion/reddit_producer.py
+++ ingestion/reddit_producer.py
@@ -1,9 +1,11 @@
 import time
 from integration.api.config import reddit_client_id, reddit_client_secret, reddit_user_agent, \
-    kafka_bootstrap_servers, crypto_list_file, reddit_posts_file
+    kafka_bootstrap_servers, crypto_list_file, last_processed_timestamp_file, topic_name
 import praw
 from kafka import KafkaProducer
 import json
+import csv
+
 
 # Setup reddit client
 reddit = praw.Reddit(
@@ -50,35 +52,63 @@ def is_crypto_related(content):
             return False
 
 
+def on_send_success(record_metadata):
+    print(f"Message sent to topic {record_metadata.topic} partition {record_metadata.partition} offset {record_metadata.offset}")
+
+
+def on_send_error(excp):
+    print(f"Error sending message: {excp}")
+
+
+# Variable to store last processed timestamp
+with open(last_processed_timestamp_file, "r") as file:
+    last_processed_timestamp = float(file.read())
+
+
 # Function to fetch and print posts related to cryptocurrencies
 def fetch_crypto_posts(subreddit_name="all", limit=10):
-    while True:
-        try:
-            for crypto in crypto_list:
-                print(f"Fetching posts related to {crypto}")
-                query = f'title:"{crypto}" OR subreddit:"{crypto}"'
+    global last_processed_timestamp
+    max_timestamp = last_processed_timestamp
+    try:
+        for crypto in crypto_list:
+            print(f"Fetching posts related to {crypto}")
+            query = f'title:"{crypto}" OR subreddit:"{crypto}"'
 
-                # Fetch posts based on the crypto keyword
-                for submission in reddit.subreddit(subreddit_name).search(query, limit=limit):
-                    # Extract relevant post information
-                    post_data = {
-                        "crypto": crypto,
-                        "title": submission.title,
-                        "subreddit": submission.subreddit.display_name,
-                        "content": submission.selftext
-                    }
+            # Fetch posts based on the crypto keyword
+            for submission in reddit.subreddit(subreddit_name).search(query, sort='new', limit=limit):
+                post_timestamp = submission.created_utc
 
-                    # Check if the post is crypto-related based on title or content
-                    if is_crypto_related(post_data['title']) or is_crypto_related(post_data['content']):
-                        # Send to kafka topic
-                        producer.send('reddit_posts_topic', post_data)
+                # Skip post if it is old
+                if post_timestamp <= last_processed_timestamp:
+                    continue
 
-            # Wait for a minute before fetching again
-            time.sleep(60)
+                # Extract relevant post information
+                post_data = {
+                    "crypto": crypto,
+                    "title": submission.title,
+                    "subreddit": submission.subreddit.display_name,
+                    "content": submission.selftext,
+                    "created_utc": submission.created_utc
+                }
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
+                # Check if the post is crypto-related based on title or content
+                if is_crypto_related(post_data['title']) or is_crypto_related(post_data['content']):
+                    # Send to kafka topic
+                    producer.send(topic_name, post_data).add_callback(on_send_success).add_errback(on_send_error)
+                    producer.flush()
+                # Update max timestamp
+                max_timestamp = max(max_timestamp, post_data['created_utc'])
+
+        with open(last_processed_timestamp_file, 'w') as file:
+            file.write(str(max_timestamp))
+
+        # Wait for a minute before fetching again
+        time.sleep(60)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    fetch_crypto_posts(limit=10)
+    while True:
+        fetch_crypto_posts(limit=10)
--- processing/sentiment_analysis.py
+++ processing/sentiment_analysis.py
@@ -1,36 +1,70 @@
-import sqlite3
-import pandas as pd
-from integration.api.config import db_name, table_name
+import time
+from integration.api.config import kafka_bootstrap_servers, topic_name, influx_db, \
+    influx_token, influx_org, influx_url
+import json
 from textblob import TextBlob
+from kafka import KafkaConsumer
+from influxdb_client import InfluxDBClient
 
-# Connect to SQLite DB
-conn = sqlite3.connect(db_name)
-query = f"SELECT * FROM {table_name}"
-df = pd.read_sql_query(query, conn)
+# Create Kafka Consumer instance
+consumer = KafkaConsumer(topic_name, bootstrap_server=kafka_bootstrap_servers,
+                         auto_offset_reset="earliest",
+                         enable_auto_commit=True,
+                         value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+
+# Influx DB client setup
+influx_client = InfluxDBClient(url=influx_url, token=influx_token, org=influx_org)
+influx_client.switch_database(influx_db)
 
 
-# Perform Sentiment Analysis
-def perform_sentiment_analysis(text):
+def analyse_sentiment(post_content):
     """
-    Performs sentiment analysis on the text
-    :param text: Post Content
-    :return: Sentiment (positive, negative or neutral)
+    Function to analyse sentiment of post
+    :param post_content: content of the post
+    :return: positive, negative or neutral based on polarity
     """
-    blob = TextBlob(text)
-    if blob.sentiment.polarity > 0:
-        return 'positive'
-    elif blob.sentiment.polarity == 0:
-        return 'neutral'
-    else:
-        return 'negative'
+    analysis = TextBlob(post_content)
+    return 'positive' if analysis.sentiment.polarity > 0 else 'negative' if analysis.sentiment.polarity < 0 \
+        else 'neutral'
 
 
-df["sentiment"] = df["content"].apply(perform_sentiment_analysis)
+def write_to_influx(crypto, sentiment, timestamp):
+    """
+    Writing sentiment data to influx db
+    :param crypto: cryptocurrency name
+    :param sentiment: positive, negative or neutral - result of sentiment analysis
+    :param timestamp: timestamp of post
+    :return: None
+    """
+    json_data = [
+        {
+            "measurement" : "crypto_sentiment",
+            "tags": {
+                "crypto": crypto,
+            },
+            "time": timestamp,
+            "fields": {
+                "sentiment": sentiment,
+            }
+        }
+    ]
+    influx_client.write_points(json_data)
 
-# Write Updated Dataframe back to SQLite db
-df.to_sql(table_name, conn, if_exists="replace", index=False)
+
+for post in consumer:
+    post_data = post.value
+    crypto = post_data.get("crypto")
+    title = post_data.get("title")
+    crypto = post_data.get("crypto")
+    content = post_data.get("content")
+    timestamp = post_data.get("created_utc")
+
+    # Perform sentiment analysis
+    sentiment = analyse_sentiment(content)
+
+    # Write Sentiment Data to Influx DB
+    write_to_influx(crypto, sentiment, timestamp)
+
+    time.sleep(1)
 
 
-# Commit transaction and Close connection
-conn.commit()
-conn.close()
